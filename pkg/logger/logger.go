610ec1ec4df8a163dbbc8ecec0aa0bd58624efad
Miguel Monzó <miguel.monzo@tecsidel.com>
2024-01-20 12:47:50 +0100

    commit init

---
+++ pkg/logger/logger.go
@@ -0,0 +1,100 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/mikemonzo/api-gateway/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger interface {
+	InitLogger()
+	Debug(args ...interface{})
+	Debugf(template string, args ...interface{})
+	Info(args ...interface{})
+	Infof(template string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(template string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(template string, args ...interface{})
+	DPanic(args ...interface{})
+	DPanicf(template string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(template string, args ...interface{})
+	Printf(template string, args ...interface{})
+}
+
+// Logger
+
+type apiLogger struct {
+	cfg         *config.Config
+	sugarLogger *zap.SugaredLogger
+}
+
+// NewAPILogger App Logger Constructor
+func NewAPILogger(cfg *config.Config) *apiLogger {
+	return &apiLogger{
+		cfg: cfg,
+	}
+}
+
+// For mapping config logger to email_service logger levels
+var loggerLevelMap = map[string]zapcore.Level{
+	"debug":  zapcore.DebugLevel,
+	"info":   zapcore.InfoLevel,
+	"warn":   zapcore.WarnLevel,
+	"error":  zapcore.ErrorLevel,
+	"dpanic": zapcore.DPanicLevel,
+	"panic":  zapcore.PanicLevel,
+	"fatal":  zapcore.FatalLevel,
+}
+
+func (l *apiLogger) getLoggerLevel(cfg *config.Config) zapcore.Level {
+	level, exist := loggerLevelMap[cfg.Logger_Level]
+
+	if !exist {
+		return zapcore.DebugLevel
+	}
+
+	return level
+}
+
+// InitLogger Initialize Logger
+func (l *apiLogger) InitLogger() {
+	logLevel := l.getLoggerLevel(l.cfg)
+
+	logWriter := zapcore.AddSync(os.Stderr)
+
+	var encoderCfg zapcore.EncoderConfig
+
+	if l.cfg.IsDevelopmentEnvironment() {
+		encoderCfg = zap.NewDevelopmentEncoderConfig()
+	} else {
+		encoderCfg = zap.NewProductionEncoderConfig()
+	}
+
+	var encoder zapcore.Encoder
+	encoderCfg.LevelKey = "LEVEL"
+	encoderCfg.CallerKey = "CALLER"
+	encoderCfg.TimeKey = "TIME"
+	encoderCfg.NameKey = "NAME"
+	encoderCfg.MessageKey = "MESSAGE"
+
+	if l.cfg.Logger_Encoding == "console" {
+		encoder = zapcore.NewConsoleEncoder(encoderCfg)
+	} else {
+		encoder = zapcore.NewJSONEncoder(encoderCfg)
+	}
+
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(encoder, logWriter, zap.NewAtomicLevelAt(logLevel))
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+
+	l.sugarLogger = logger.Sugar()
+	if err := l.sugarLogger.Sync(); err != nil {
+		l.sugarLogger.Error(err)
+	}
+}
+
+// Logger methods
